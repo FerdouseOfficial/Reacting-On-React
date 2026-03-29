c6a4cf9785637bb1da3014b5aa5e1bcae9ac897b
FerdouseOfficial <mollajihad402@gmail.com>
2026-03-29 19:45:56 +0600

    Added new changes

--- Practice/src/components/ProductInfo.jsx
+++ Practice/src/components/ProductInfo.jsx
@@ -4,12 +4,18 @@ const ProductInfo = () => {
   //   let name = "Laptop";
   //   let price = "$1200";
   //   let availAbility = "In stock";
-  let product = ["Laptop", "1200", "In stock"];
+  //   let product = ["Laptop", "1200", "In stock"]; // Doing using Array
+  let product = {
+    // Doing using Object
+    name: "Laptop",
+    price: "1200",
+    availibility: "In stock",
+  };
   return (
     <div className="flex flex-col justify-center items-center h-[90vh] gap-4">
-      <h2 className="text-3xl uppercase"> {product[0]} </h2>
-      <p className="font-semibold"> ${product[1]} </p>
-      <p className="tracking-wider font-extrabold"> {product[2]} </p>
+      <h2 className=" ">Name : {product.name} </h2>
+      <h2 className=""> Price: ${product.price} </h2>
+      <h2 className="tracking-wider">Availibility: {product.availibility}</h2>
     </div>
   );
 };
