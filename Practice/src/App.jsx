1a7251ac93fb7d2a08c03df7c6fc32332b7b3dd6
FerdouseOfficial <mollajihad402@gmail.com>
2026-03-29 19:04:18 +0600

    Finishing ex 3 and 4

--- Practice/src/App.jsx
+++ Practice/src/App.jsx
@@ -3,14 +3,16 @@ import Greet from "./components/Greet";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
+import WelcomeMessage from "./components/WelcomeMessage";
 
 const App = () => {
   return (
     <div>
-      <Greet />
+      {/* <Greet />
       <Header />
       <MainContent />
-      <Footer />
+      <Footer /> */}
+      <WelcomeMessage/>  sade gamsa longi ase
     </div>
   );
 };
