e63901caa07b302add2c121b0d1a328368ee3301
FerdouseOfficial <mollajihad402@gmail.com>
2026-03-29 19:25:01 +0600

    practicing the exercise of the expression using kerly braces

--- Practice/src/components/Greet.jsx
+++ Practice/src/components/Greet.jsx
@@ -9,15 +9,31 @@
 // }
 
 // export default Greet;
+// import React from "react";
+
+// const Greet = () => {
+//   return (
+//     <div>
+//       <h2>Hello Everyone</h2>
+//     </div>
+//   );
+// };
+
+// export default Greet;
+
 import React from "react";
 
 const Greet = () => {
+  let greet = "Hello Everyone I am Jihad";
+  let intro = "I am a billionaire. ";
+  let date =  new Date();
   return (
-    <div>
-      <h2>Hello Everyone</h2>
+    <div className="flex flex-col justify-center items-center h-[90vh] ">
+      <h1>{greet}</h1>
+      {/* <p>{intro}</p> */}
+      <p>{date.toDateString()}</p>
     </div>
   );
 };
 
 export default Greet;
- 
