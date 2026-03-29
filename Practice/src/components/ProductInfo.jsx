95f59815c6cb5f5c1074652d156360cbc79a3d30
FerdouseOfficial <mollajihad402@gmail.com>
2026-03-29 19:36:29 +0600

    The exercise is finished

--- Practice/src/components/ProductInfo.jsx
+++ Practice/src/components/ProductInfo.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 
 const ProductInfo = () => {
-  let name = "Laptop";
-  let price = "$1200";
-  let availAbility = "In stock";
+  //   let name = "Laptop";
+  //   let price = "$1200";
+  //   let availAbility = "In stock";
+  let product = ["Laptop", "1200", "In stock"];
   return (
     <div className="flex flex-col justify-center items-center h-[90vh] gap-4">
-      <h2 className="text-3xl uppercase"> {name} </h2>
-      <p className="font-semibold"> {price} </p>
-      <p className="tracking-wider font-extrabold"> {availAbility} </p>
+      <h2 className="text-3xl uppercase"> {product[0]} </h2>
+      <p className="font-semibold"> ${product[1]} </p>
+      <p className="tracking-wider font-extrabold"> {product[2]} </p>
     </div>
   );
 };
