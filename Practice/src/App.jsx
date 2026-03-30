7381bdd5c34db222bd010409d84a0784c7f0f7d6
FerdouseOfficial <bsource@gmail.com>
2026-03-31 00:10:24 +0600

    Finished setting up the Practice2 folder cause the practice folder got so bigger and messy that is why I need to create another one

--- Practice/src/App.jsx
+++ Practice/src/App.jsx
@@ -1,91 +1,92 @@
-// import React from "react";
-// import Greet from "./components/Greet";
-// import Header from "./components/Header";
-// import MainContent from "./components/MainContent";
-// import Footer from "./components/Footer";
-// import WelcomeMessage from "./components/WelcomeMessage";
-// import JSXRules from "./components/JSXRules";
-// import ProductInfo from "./components/ProductInfo";
-// import Pr_Expressions from "./components/Pr_Expressions";
+// // import React from "react";
+// // import Greet from "./components/Greet";
+// // import Header from "./components/Header";
+// // import MainContent from "./components/MainContent";
+// // import Footer from "./components/Footer";
+// // import WelcomeMessage from "./components/WelcomeMessage";
+// // import JSXRules from "./components/JSXRules";
+// // import ProductInfo from "./components/ProductInfo";
+// // import Pr_Expressions from "./components/Pr_Expressions";
+
+// // // const App = () => {
+// // //   return (
+// // //     // <div>
+// // //     //   {/* <Greet />
+// // //     //   <Header />
+// // //     //   <MainContent />
+// // //     //   <Footer /> */}
+// // //     //   {/* <WelcomeMessage/>  */}
+// // //     //   <JSXRules/>
+// // //     // </div>
+
+// // //     <div>
+// // //       {/* <h2>2+2 is {2 + 2}</h2> */}
+// // //       {/* <Greet /> */}
+// // //       {/* <ProductInfo/> */}
+// // //     </div>
+// // //   );
+// // // };
 
 // // const App = () => {
-// //   return (
-// //     // <div>
-// //     //   {/* <Greet />
-// //     //   <Header />
-// //     //   <MainContent />
-// //     //   <Footer /> */}
-// //     //   {/* <WelcomeMessage/>  */}
-// //     //   <JSXRules/>
-// //     // </div>
+// //   let list = [1, 2, 3, 4, 5];
+// //   //   return (
+// //   //     <div className="flex flex-col items-center">
+// //   //       {list.map((number) => (
+// //   //         <ul key={number}>
+// //   //           <li>{number}</li>
+// //   //         </ul>
+// //   //       ))}
+// //   //     </div>
 
+// //   // );
+// //   return (
 // //     <div>
-// //       {/* <h2>2+2 is {2 + 2}</h2> */}
-// //       {/* <Greet /> */}
-// //       {/* <ProductInfo/> */}
+// //       <Pr_Expressions />
 // //     </div>
 // //   );
 // // };
 
-// const App = () => {
-//   let list = [1, 2, 3, 4, 5];
-//   //   return (
-//   //     <div className="flex flex-col items-center">
-//   //       {list.map((number) => (
-//   //         <ul key={number}>
-//   //           <li>{number}</li>
-//   //         </ul>
-//   //       ))}
-//   //     </div>
+// // export default App;
 
-//   // );
+// import React from "react";
+// import Pr_Expressions from "./components/Pr_Expressions";
+// import UserList from "./components/UserList";
+// import ProductLists from "./components/ProductLists";
+// import Person from "./components/Person";
+// import Product from "./components/Product";
+// import Verification from "./components/Verification";
+// import Cart from "./components/Cart";
+// import Temperature from "./components/Temperature";
+// import UserStatus from "./components/UserStatus";
+// import Greeting from "./components/Greeting";
+// import StyledCard from "./components/StyledCard";
+// import ProfileCard from "./components/ProfileCard";
+// import IconComponent from "./components/IconComponent";
+// import Button from "./components/Button";
+
+// const App = () => {
 //   return (
-//     <div>
-//       <Pr_Expressions />
-//     </div>
+//     // <div>
+//     //   {/* <Pr_Expressions/> */}
+//     //   {/* <UserList/>
+//     //   <ProductLists/> */}
+//     //   {/* <Person name="Jihad" age="18" />
+//     //   <Product name="Laptop" price="1299" /> */}
+
+//     //   {/* <Verification isValid={true}/>  */}
+//     //     {/* <Cart/> */}
+//     //     {/* <Temperature temp="24" /> */}
+//     // </div>
+//     // <div>
+//     //   {/* <UserStatus isloggedIn={true} isAdmin={false} /> */}
+//     //   {/* <Greeting timeOfDay="morning" /> */}
+//     //   {/* <StyledCard/> */}
+//     //   {/* <ProfileCard/> */}
+//     //   {/* <IconComponent /> */}
+//     //   <Button />
+//     // </div>
+//     <Button/>
 //   );
 // };
 
 // export default App;
-
-import React from "react";
-import Pr_Expressions from "./components/Pr_Expressions";
-import UserList from "./components/UserList";
-import ProductLists from "./components/ProductLists";
-import Person from "./components/Person";
-import Product from "./components/Product";
-import Verification from "./components/Verification";
-import Cart from "./components/Cart";
-import Temperature from "./components/Temperature";
-import UserStatus from "./components/UserStatus";
-import Greeting from "./components/Greeting";
-import StyledCard from "./components/StyledCard";
-import ProfileCard from "./components/ProfileCard";
-import IconComponent from "./components/IconComponent";
-import Button from "./components/Button";
-
-const App = () => {
-  return (
-    // <div>
-    //   {/* <Pr_Expressions/> */}
-    //   {/* <UserList/>
-    //   <ProductLists/> */}
-    //   {/* <Person name="Jihad" age="18" />
-    //   <Product name="Laptop" price="1299" /> */}
-
-    //   {/* <Verification isValid={true}/>  */}
-    //     {/* <Cart/> */}
-    //     {/* <Temperature temp="24" /> */}
-    // </div>
-    <div>
-      {/* <UserStatus isloggedIn={true} isAdmin={false} /> */}
-      {/* <Greeting timeOfDay="morning" /> */}
-      {/* <StyledCard/> */}
-      {/* <ProfileCard/> */}
-      {/* <IconComponent /> */}
-      <Button />
-    </div>
-  );
-};
-
-export default App;
--- Practice/src/components/Button.jsx
+++ Practice/src/components/Button.jsx
@@ -4,6 +4,7 @@ const Button = () => {
   const handleClick = () => console.log(Math.round(Math.random() * 10));
   return (
     <div>
+      <h1>Hello</h1>
       <button onClick={handleClick}>Click</button>
     </div>
   );
