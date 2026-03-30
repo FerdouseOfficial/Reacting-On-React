ce009299ca5e400db3b43509e7905e718e64f64c
FerdouseOfficial <mollajihad402@gmail.com>
2026-03-30 13:05:07 +0600

    I have solve the Product.jsx but some minor changes I have done it with the same both 2 version one with props and another with js destructuring okay

--- Practice/src/components/Product.jsx
+++ Practice/src/components/Product.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+// import React from 'react'
 
-const Product = (props) => {
+// const Product = (props) => {
+//   return (
+//     <div>
+//         <h2>Name: {props.name} </h2>
+//         <h2>Price: {props.price} </h2>
+//     </div>
+//   )
+// }
+
+// export default Product
+
+// Doing it using JS Destructuring
+
+import React from "react";
+
+const Product = ({ name, price }) => {
   return (
     <div>
-        <h2>Name: {props.name} </h2>
-        <h2>Price: {props.price} </h2>
+      <h2>Name: {name} </h2>
+      <h2>Price: {price} </h2>
     </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
