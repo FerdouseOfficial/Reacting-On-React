c11abfe62ba1b67a750eae7227179a26a19fe4ff
FerdouseOfficial <bsource@gmail.com>
2026-03-30 21:18:43 +0600

    Started the concept of events

--- Practice/src/components/IconComponent.jsx
+++ Practice/src/components/IconComponent.jsx
@@ -1,21 +1,34 @@
-import React from "react";
-import { FaBeerMugEmpty } from "react-icons/fa6";
+// import React from "react";
+// import { FaBeerMugEmpty } from "react-icons/fa6";
+
+// const IconComponent = () => {
+//   return (
+//     <div
+//       style={{
+//         display: "flex",
+//         justifyContent: "center",
+//         alignItems: "center",
+//         height: "80vh",
+//         fontSize:"30px",
+//         color: "gold",
+//       }}
+//     >
+//       <FaBeerMugEmpty />
+//     </div>
+//   );
+// };
+
+// export default IconComponent;
+
+import React from 'react'
 
 const IconComponent = () => {
+ cons
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "80vh",
-        fontSize:"30px",
-        color: "gold",
-      }}
-    >
-      <FaBeerMugEmpty />
-    </div>
-  );
-};
+    <div>
 
-export default IconComponent;
+    </div>
+  )
+}
+
+export default IconComponent
