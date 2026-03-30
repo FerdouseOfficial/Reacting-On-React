c5b10e196e6fe54b5920f9208a4a4a39c4ec3eab
FerdouseOfficial <mollajihad402@gmail.com>
2026-03-30 12:22:19 +0600

    solving the problem name Rendering a list of Data with .map()

---
+++ Practice/src/components/UserList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UserList = () => {
+  const user = [
+    { id: 1, name: "Alice", age: 25 },
+    { id: 2, name: "Bob", age: 30 },
+    { id: 3, name: "Charlie", age: 22 },
+  ];
+  return <div></div>;
+};
+
+export default UserList;
