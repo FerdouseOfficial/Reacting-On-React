708f07fff25b3ef7fda837fc246585476d0769ad
FerdouseOfficial <mollajihad402@gmail.com>
2026-03-30 12:17:54 +0600

    solved a tiny problem using Javascript destructuring

--- Practice/src/App.jsx
+++ Practice/src/App.jsx
@@ -1,42 +1,64 @@
-import React from "react";
-import Greet from "./components/Greet";
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
-import Footer from "./components/Footer";
-import WelcomeMessage from "./components/WelcomeMessage";
-import JSXRules from "./components/JSXRules";
-import ProductInfo from "./components/ProductInfo";
+// import React from "react";
+// import Greet from "./components/Greet";
+// import Header from "./components/Header";
+// import MainContent from "./components/MainContent";
+// import Footer from "./components/Footer";
+// import WelcomeMessage from "./components/WelcomeMessage";
+// import JSXRules from "./components/JSXRules";
+// import ProductInfo from "./components/ProductInfo";
+// import Pr_Expressions from "./components/Pr_Expressions";
+
+// // const App = () => {
+// //   return (
+// //     // <div>
+// //     //   {/* <Greet />
+// //     //   <Header />
+// //     //   <MainContent />
+// //     //   <Footer /> */}
+// //     //   {/* <WelcomeMessage/>  */}
+// //     //   <JSXRules/>
+// //     // </div>
+
+// //     <div>
+// //       {/* <h2>2+2 is {2 + 2}</h2> */}
+// //       {/* <Greet /> */}
+// //       {/* <ProductInfo/> */}
+// //     </div>
+// //   );
+// // };
 
 // const App = () => {
-//   return (
-//     // <div>
-//     //   {/* <Greet />
-//     //   <Header />
-//     //   <MainContent />
-//     //   <Footer /> */}
-//     //   {/* <WelcomeMessage/>  */}
-//     //   <JSXRules/>
-//     // </div>
+//   let list = [1, 2, 3, 4, 5];
+//   //   return (
+//   //     <div className="flex flex-col items-center">
+//   //       {list.map((number) => (
+//   //         <ul key={number}>
+//   //           <li>{number}</li>
+//   //         </ul>
+//   //       ))}
+//   //     </div>
 
+//   // );
+//   return (
 //     <div>
-//       {/* <h2>2+2 is {2 + 2}</h2> */}
-//       {/* <Greet /> */}
-//       {/* <ProductInfo/> */}
+//       <Pr_Expressions />
 //     </div>
 //   );
 // };
 
-const App = () => {
-  let list = [1, 2, 3, 4, 5];
-  return (
-    <div className="flex flex-col items-center">
-      {list.map((number) => (
-        <ul key={number}>
-          <li>{number}</li>
-        </ul>
-      ))}
-    </div>
-  );
-};
+// export default App;
 
-export default App;
+
+
+import React from 'react'
+import Pr_Expressions from './components/Pr_Expressions'
+
+const App = () => {
+  return (
+    <div>
+      <Pr_Expressions/>
+    </div>
+  )
+}
+
+export default App
--- Practice/src/components/Pr_Expressions.jsx
+++ Practice/src/components/Pr_Expressions.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
 
 const Pr_Expressions = () => {
+  const usersInfo = [
+    {
+      username: "Jihad",
+      email: "Jihad@gmail.com",
+      location: "Bangladesh",
+    },
+    {
+      username: "Mijanur",
+      email: "Mijanur@gmail.com",
+      location: "India",
+    },
+    {
+      username: "Ripon",
+      email: "Ripon1@gmail.com",
+      location: "Gulistan",
+    },
+  ];
   return (
-    <div>
-        
+    <div className="flex  items-center justify-center h-[50vh] gap-7 ">
+      {usersInfo.map(({ username, email, location }) => {
+        // doing it using destructuring
+        return (
+          <ul key={Math.random()} className="list-disc">
+            <li> {username} </li>
+            <li> {email} </li>
+            <li> {location} </li>
+          </ul>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default Pr_Expressions
+export default Pr_Expressions;
