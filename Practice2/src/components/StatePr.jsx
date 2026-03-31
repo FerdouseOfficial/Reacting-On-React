18af0ac1e3d576528f02af10b2c45dbd0a84462d
FerdouseOfficial <bsource@gmail.com>
2026-03-31 13:44:59 +0600

    Rest time is now Baby start working

--- Practice2/src/components/StatePr.jsx
+++ Practice2/src/components/StatePr.jsx
@@ -1,16 +1,38 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+
+// const StatePr = () => {
+//   const [count, setCount] = useState(0);
+//   const increase = () => setCount(count + 1);
+//   const decrease = () => setCount(count - 1);
+//   return (
+//     <div>
+//       <h1 className="px-4 py-2  ml-17 text-6xl">{count}</h1>
+//       <button
+//         onClick={increase}
+//         className="px-4 py-2 bg-amber-500 m-7 cursor-pointer"
+//       >
+//         +
+//       </button>
+//       <button
+//         onClick={decrease}
+//         className="px-4 py-2 bg-amber-500 m-7 cursor-pointer"
+//       >
+//         -
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default StatePr;
+
+// Practice useState on Array
+
+import React from "react";
 
 const StatePr = () => {
-  const [count, setCount] = useState(0);
-  const increase = () => setCount(count + 1);
-  return (
-    <div>
-      <h1 className="px-4 py-2 m-7">{count}</h1>
-      <button onClick={increase} className="px-4 py-2 bg-amber-500 m-7">
-        +
-      </button>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default StatePr;
+
+// Practice useState on Object
