580f2c7461ed13e5ff495037c02c05972527f424
FerdouseOfficial <bsource@gmail.com>
2026-04-01 19:40:06 +0600

    hey rest time started lets goo boom

--- Practice2/src/App.jsx
+++ Practice2/src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
       {/* <StatePr/> */}
       {/* <State/> */}
       {/* <Movie/> */}
-      <Example/>
+      <Example/>  
     </div>
   );
 };
--- Practice2/src/components/Example.jsx
+++ Practice2/src/components/Example.jsx
@@ -1,21 +1,44 @@
+// import React, { useState } from "react";
+
+// const Example = () => {
+//   const [randomNumber, setRandomNumber] = useState(() => {
+//     return Math.floor(Math.random() * 100);
+//   });
+//   const generateNewRandomNumber = () => {
+//     setRandomNumber(Math.floor(Math.random() * 100));
+//   };
+//   return (
+//     <div>
+//       <h1>Random Number:- {randomNumber}</h1>
+//       <button
+//         onClick={generateNewRandomNumber}
+//         className=" px-4 py-2 bg-amber-500 rounded font-semibold m-2 cursor-pointer"
+//       >
+//         Generate New Random Number
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default Example;
+
 import React, { useState } from "react";
 
 const Example = () => {
-  const [randomNumber, setRandomNumber] = useState(() => {
-    return Math.floor(Math.random() * 100);
-  });
-  const generateNewRandomNumber = () => {
-    setRandomNumber(Math.floor(Math.random() * 100));
+  const [boom, setBoom] = useState("You are not going forgot me");
+  const boomb = () => {
+    let tnt = document.querySelector("h1").innerHTML("Boom");
+    setBoom(tnt);
   };
   return (
-    <div>
-      <h1>Random Number:- {randomNumber}</h1>
-      <button
-        onClick={generateNewRandomNumber}
-        className=" px-4 py-2 bg-amber-500 rounded font-semibold m-2 cursor-pointer"
+    <div className="flex flex-col justify-center items-center h-[70vh]">
+      <h1>{boom}</h1>
+      <buttons
+        onClick="boomb"
+        className="px-4 py-2 bg-amber-300 roudded m-2 cursor-pointer"
       >
-        Generate New Random Number
-      </button>
+        Boom
+      </buttons>
     </div>
   );
 };
