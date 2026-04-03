ad80c63c1056a21ca22fbcfbdf5162d400a99b16
FerdouseOfficial <bsource@gmail.com>
2026-04-04 00:47:58 +0600

    Creating a Complex UI that will need some work and plan At firs I will do that and then I will come here and start to make it okay

--- Practice2/src/components/Navbar.jsx
+++ Practice2/src/components/Navbar.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
     <div>
-       <div className=''>
-        </div> 
+      <div className="flex justify-between items-center p-10 m-2">
+        {" "}
+        {/* please do it for me. Fetch data of forbes litst using API in react use tailwind to design also add you name on the Billionaire list. Although You are Billionaire okay */}
+        <div>
+          <h1>Billionaire Jihad</h1>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
