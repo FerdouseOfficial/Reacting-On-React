5da4634074284f903e28d1d5cc6c8430d36e9c16
FerdouseOfficial <bsource@gmail.com>
2026-04-04 00:43:49 +0600

    Adding everything and setting up the process to go and look through it properly

--- Practice2/src/components/Header.jsx
+++ Practice2/src/components/Header.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./Navbar";
 
 const Header = () => {
   return (
     <div>
-        
+      <Navbar /> {/* Adding Navbar Section here */}
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
