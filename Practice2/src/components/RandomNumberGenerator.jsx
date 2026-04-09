d8cc25dea46082d3c7663dad74244ae233dad5f0
FerdouseOfficial <bsource96@gmail.com>
2026-04-09 17:24:34 +0600

    Setting up the environment and layout

--- Practice2/src/components/RandomNumberGenerator.jsx
+++ Practice2/src/components/RandomNumberGenerator.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const RandomNumberGenerator = () => {
   return (
-    <div >
-        <h1>Random Number: </h1>
-        <button>Generatore Number</button>
+    <div className="flex flex-col justify-center items-center h-[80vh]">
+      <h1 className="text-2xl font-extrabold">Random Number: </h1>
+      <button className="px-4 py-2 mt-5 rounded">Generatore Number</button>
     </div>
-  )
-}
+  );
+};
 
-export default RandomNumberGenerator
+export default RandomNumberGenerator;
