a93d8bf05139e3c9f593a56b0b31e0c7f2345d23
FerdouseOfficial <bsource96@gmail.com>
2026-04-09 17:36:19 +0600

    Its finished just some tiny fixes on the way

--- Practice2/src/components/RandomNumberGenerator.jsx
+++ Practice2/src/components/RandomNumberGenerator.jsx
@@ -3,13 +3,11 @@ import React, { useState } from "react";
 const RandomNumberGenerator = () => {
   const [randomNumber, setRandomNumber] = useState(0);
   const RandomNumberGen = () => {
-    setRandomNumber(Math.round(Math.random() * 1000));
+    setRandomNumber(Math.floor(Math.random() * 100));
   };
   return (
     <div className="flex flex-col justify-center items-center h-[80vh]">
-      <h1 className="text-2xl font-extrabold">
-        Random Number: {randomNumber}{" "}
-      </h1>
+      <h1 className="text-2xl font-extrabold">Random Number: {randomNumber}</h1>
       <button
         onClick={RandomNumberGen}
         className="px-4 py-2 mt-5 bg-amber-500 rounded cursor-pointer"
@@ -19,6 +17,5 @@ const RandomNumberGenerator = () => {
     </div>
   );
 };
-s;
 
 export default RandomNumberGenerator;
